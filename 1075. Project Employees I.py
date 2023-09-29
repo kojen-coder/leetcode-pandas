5d168a364492ab82bf364eb696388cb9bce5ba20
karenwang <kojenwang7@gmail.com>
2023-09-29 00:06:15 -0400

    agg()

---
+++ 1075. Project Employees I.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def project_employees_i(project: pd.DataFrame, employee: pd.DataFrame) -> pd.DataFrame:
+    df = project.merge(employee, on='employee_id', how='inner').reset_index()
+    return df.groupby('project_id').agg(average_years=('experience_years', lambda x: round(x.mean(), 2))).reset_index()
